55c45cc170087b9f215c3a6cff817f9e6a5446eb
zzprimal <devonragosin@gmail.com>
2025-03-07 21:46:24 -0700

    new database stuff

--- code/app/src/androidTest/java/com/example/androidcicd/MainActivityTest.java
+++ code/app/src/androidTest/java/com/example/androidcicd/MainActivityTest.java
@@ -155,7 +155,7 @@ public class MainActivityTest {
     }
     @After
     public void tearDown() {
-        String projectId = "lab8-9b463";
+        String projectId = "lab08-d38c0";
         URL url = null;
         try {
             url = new URL("http://10.0.2.2:8080/emulator/v1/projects/" + projectId + "/databases/(default)/documents");
